b9bb4022eefe11e45adf474450df41b042132b9b
otarbakh <otarbakh@gmail.com>
2023-09-05 22:52:54 +0400

    Apllication works properly :)

--- app/src/main/java/com/otarbakh/andersen_task__1/MainActivity.kt
+++ app/src/main/java/com/otarbakh/andersen_task__1/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.otarbakh.andersen_task__1
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.otarbakh.andersen_task__1.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val editText = binding.edittext.text.toString()
+
+            if (isValidUrl(editText)){
+
+                Glide.with(this)
+                    .load(editText)
+                    .into(binding.image)
+            }else
+            {
+                Toast.makeText(this,"This is Not LINK ! ! !", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun isValidUrl(url: String): Boolean {
+
+        return url.startsWith("http://") || url.startsWith("https://")
     }
 }
+
+
+
+
+
